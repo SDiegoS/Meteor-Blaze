13ddae7b4a885011cc758f4a544332a24faeeeb2
Diego Ireno dos Santos <diego_ireno@hotmail.com>
2020-06-17 10:28:51 -0300

    [UPD] - Continuação do curso.

--- simple-todos/imports/ui/body.js
+++ simple-todos/imports/ui/body.js
@@ -6,6 +6,30 @@ import './body.html';
 
 Template.body.helpers({
     tasks() {
-        return Tasks.find({});
+        return Tasks.find({}, {
+            // Mostra as tarefas mais recentes na parte superior
+            sort: {createdAt: -1}
+        });
     },
 });
+
+Template.body.events({
+    'submit .new-task'(event) {
+        console.log(event);
+        //Impedir o envio do formulário padrão do navegador
+        event.preventDefault();
+
+        // Obter valor do elemento do formulário
+        const target = event.target;
+        const text = target.text.value;
+
+        // Insere uma tarefa na coleção
+        Tasks.insert({
+            text,
+            createdAt: new Date(),
+        });
+
+        // Forma limpa
+        target.text.value = '';
+    },
+})
