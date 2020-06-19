2a86d1e43b4dbed0a3b7c024b0c357073e6d4270
Diego Ireno dos Santos <diego_ireno@hotmail.com>
2020-06-19 09:40:18 -0300

    criando base

--- simple-todos/imports/ui/body.js
+++ simple-todos/imports/ui/body.js
@@ -1,55 +1,11 @@
 import { Template } from 'meteor/templating';
-import { ReactiveDict } from 'meteor/reactive-dict';
 
 import { Tasks } from '../api/tasks.js';
 
-import  './task.js';
 import './body.html';
-
-Template.body.onCreated(function bodyOnCreated() {
-    this.state = new ReactiveDict();
-    Meteor.subscribe('tasks');
-});
 
 Template.body.helpers({
     tasks() {
-        const instance = Template.instance();
-        if (instance.state.get('hideCompleted')) {
-            // Se ocultar concluído estiver marcado, filtre as tarefas
-            return Tasks.find({ checked: {$ne: true}}, { sort: {createdAt: -1}});
-        }
-
-        return Tasks.find({}, {
-            // Caso contrário, retorne todas as tarefas
-            sort: {createdAt: -1}
-        });
-    },
-    incompleteCount() {
-        return Tasks.find({ checked: {$ne: true}}).count();
+        return Tasks.find({});
     },
 });
-
-Template.body.events({
-    'click .delete-all-checked'() {
-        Meteor.call('tasks.delete-all-checked');
-    },
-    'submit .new-task'(event) {
-        //Impedir o envio do formulário padrão do navegador
-        event.preventDefault();
-
-        // Obter valor do elemento do formulário
-        const target = event.target;
-        const text = target.text.value;
-        const description = target.description.value;
-
-
-        Meteor.call('tasks.insert', text, description);
-
-        // Forma limpa
-        target.text.value = '';
-        target.description.value = '';
-    },
-    'change .hide-completed input'(event, instance) {
-        instance.state.set('hideCompleted', event.target.checked);
-    },
-})
--- simple-todos/tests/main.js
+++ simple-todos/tests/main.js
@@ -1,5 +1,4 @@
 import assert from "assert";
-import '../imports/api/tasks.tests';
 
 describe("simple-todos", function () {
   it("package.json has correct name", async function () {
